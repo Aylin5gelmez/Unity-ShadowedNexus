354eb8b0e52a3e05aa5815c2d34e2c7ef29e3a82
AlperenTrgt <130250411+AlperenTrgt@users.noreply.github.com>
2023-07-15 17:15:59 +0300

    Eklemeler.
    Kapıların açılması, Arkaplan sesi, yürüme sesi, kapı sesleri ve ana mape bazı eklemeler yapıldı.

--- Unity-ShadowedNexus/Assets/Scripts/FPSMovement.cs
+++ Unity-ShadowedNexus/Assets/Scripts/FPSMovement.cs
@@ -1,3 +1,75 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+    public class FPSMovement : MonoBehaviour
+    {
+        public Camera playerCamera;
+        public float walkSpeed = 3f;
+        public float gravity = 10f;
+        public float lookSpeed = 2f;
+        public float lookXLimit = 45f;
+        public AudioSource footstep;
+
+
+
+    Vector3 moveDirection = Vector3.zero;
+        float rotationX = 0;
+        
+        public bool canMove = true;
+
+
+        CharacterController characterController;
+        void Start()
+        {
+            characterController = GetComponent<CharacterController>();
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        void Update()
+        {
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+
+            float curSpeedX;
+            float curSpeedY;
+    
+            if (canMove)
+                {
+                    curSpeedX = walkSpeed * Input.GetAxis("Vertical");
+                    curSpeedY = walkSpeed * Input.GetAxis("Horizontal");
+                }   
+                else
+                {
+                    curSpeedX = 0;
+                    curSpeedY = 0;
+                }
+
+            float movementDirectionY = moveDirection.y;
+            moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        moveDirection.y = movementDirectionY;
+    
+            if (!characterController.isGrounded)
+            {
+                moveDirection.y -= gravity * Time.deltaTime;
+            }
+    
+    
+            characterController.Move(moveDirection * Time.deltaTime);
+    
+            if (canMove)
+            {
+                rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+                rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+                playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+                transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+
+        }
+    }
+    }*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +79,12 @@ using UnityEngine;
 public class FPSMovement : MonoBehaviour
 {
     public Camera playerCamera;
-    public float walkSpeed = 6f;
-    public float runSpeed = 12f;
-    public float jumpPower = 7f;
+    public float walkSpeed = 3f;
     public float gravity = 10f;
-
-
     public float lookSpeed = 2f;
     public float lookXLimit = 45f;
+    public AudioSource footstep; // make sure to assign an audio clip to this in the inspector
+
 
 
     Vector3 moveDirection = Vector3.zero;
@@ -35,22 +105,24 @@ public class FPSMovement : MonoBehaviour
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
 
-        // Press Left Shift to run
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
-        float movementDirectionY = moveDirection.y;
-        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+        float curSpeedX;
+        float curSpeedY;
 
-
-        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        if (canMove)
         {
-            moveDirection.y = jumpPower;
+            curSpeedX = walkSpeed * Input.GetAxis("Vertical");
+            curSpeedY = walkSpeed * Input.GetAxis("Horizontal");
         }
         else
         {
-            moveDirection.y = movementDirectionY;
+            curSpeedX = 0;
+            curSpeedY = 0;
         }
+
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        moveDirection.y = movementDirectionY;
 
         if (!characterController.isGrounded)
         {
@@ -66,6 +138,20 @@ public class FPSMovement : MonoBehaviour
             rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+
+            // play footstep sound if moving
+            if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+            {
+                if (!footstep.isPlaying)
+                {
+                    footstep.Play();
+                }
+            }
+            else
+            {
+                // stop footstep sound if not moving
+                footstep.Stop();
+            }
         }
     }
 }
